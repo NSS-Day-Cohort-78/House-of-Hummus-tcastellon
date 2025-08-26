b0414c4de14b676c4e28d615ed965b8f0c0fb0f0
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-26 13:39:10 -0500

    Updated food truck module

--- scripts/FoodTruck.js
+++ scripts/FoodTruck.js
@@ -1,22 +1,36 @@
-import { Sales } from "./Sales.js"
+// import { Sales } from "./Sales.js"
+import { entreeOptions } from "./Entrees.js"
 
-export const FoodTruck = () => {
-    const salesHTML = Sales()
+export const FoodTruck = async() => {
+    // const salesHTML = Sales()
+    const entreesHTML = await entreeOptions()
 
     return `
         <header class="header">
             <img src="./images/hummus.png" class="logo" />
             <h1 class="title">Laura Kathryn's House of Hummus</h1>
         </header>
-
+        <article class="selections">
+            <div class="base-dish">
+                <h3>Base Dish</h3>
+                ${entreesHTML}
+            </div>
+            <div class="veggie-dish">
+                <h3>Vegetable</h3>
+            </div>
+            <div class="side-dish">
+                <h3>Sides</h3>
+            </div>
+        </article>
         <article>
             <button id="purchase">Purchase Combo</button>
         </article>
 
         <article class="customerOrders">
             <h2>Monthly Sales</h2>
-            ${salesHTML}
+            
         </article>
 
     `
 }
+// ${salesHTML} moved from monthly sales

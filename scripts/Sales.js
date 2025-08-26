1a2cafedb13e34099c23d6982752358831b7097a
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-26 13:05:08 -0500

    Updated Sales

--- scripts/Sales.js
+++ scripts/Sales.js
@@ -1,7 +1,7 @@
 export const Sales = async () => {
-    const sales = await fetch("http://localhost:8088/orders").then(res => res.json())
+    const sales = await fetch("http://localhost:8088/purchases").then(res => res.json())
 
-    let salesDivs = sales.map()
+    let salesDivs = sales.map(sale)
 
     salesDivs = salesDivs.join("")
 
